32920a257f4a1d6e6902fc4de152e96cedb4846a
Phoebe890 <123245247+Phoebe890@users.noreply.github.com>
2025-06-18 12:03:45 +0300

    made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
  template: `
-    <h1>Hello, {{ title }}</h1>
+    
     <router-outlet></router-outlet>
   `,
   styles: [],
